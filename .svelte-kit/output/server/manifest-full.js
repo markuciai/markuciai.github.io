bcf264500f1aa30a3c0473f4cace732f2815d3c6
Čhepulis <chepulis@gmail.com>
2023-03-29 07:56:27 +0300

    favicon

--- .svelte-kit/output/server/manifest-full.js
+++ .svelte-kit/output/server/manifest-full.js
@@ -1,10 +1,10 @@
 export const manifest = {
 	appDir: "_app",
 	appPath: "_app",
-	assets: new Set(["favicon.png"]),
+	assets: new Set([".DS_Store","favicon.png"]),
 	mimeTypes: {".png":"image/png"},
 	_: {
-		client: {"start":{"file":"_app/immutable/entry/start.1d773c01.js","imports":["_app/immutable/entry/start.1d773c01.js","_app/immutable/chunks/index.776eae6c.js","_app/immutable/chunks/singletons.282ac8d5.js","_app/immutable/chunks/paths.3ffc0376.js"],"stylesheets":[],"fonts":[]},"app":{"file":"_app/immutable/entry/app.8eece587.js","imports":["_app/immutable/entry/app.8eece587.js","_app/immutable/chunks/index.776eae6c.js"],"stylesheets":[],"fonts":[]}},
+		client: {"start":{"file":"_app/immutable/entry/start.0f7024d7.js","imports":["_app/immutable/entry/start.0f7024d7.js","_app/immutable/chunks/index.776eae6c.js","_app/immutable/chunks/singletons.56b4bb71.js","_app/immutable/chunks/paths.6a9f536e.js"],"stylesheets":[],"fonts":[]},"app":{"file":"_app/immutable/entry/app.96d04ee5.js","imports":["_app/immutable/entry/app.96d04ee5.js","_app/immutable/chunks/index.776eae6c.js"],"stylesheets":[],"fonts":[]}},
 		nodes: [
 			() => import('./nodes/0.js'),
 			() => import('./nodes/1.js'),
--- docs/_app/immutable/chunks/1.244230ea.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.53e785f3.js";export{t as component};
