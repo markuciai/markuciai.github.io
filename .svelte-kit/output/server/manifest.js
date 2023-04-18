ed55cb2c68f6ce52c3683e7fd1104fb3100811d2
Čhepulis <chepulis@gmail.com>
2023-04-18 22:51:03 +0300

    user-select police
    wee woo wee woo

--- .svelte-kit/output/server/manifest.js
+++ .svelte-kit/output/server/manifest.js
@@ -4,7 +4,7 @@ export const manifest = {
 	assets: new Set([".DS_Store","favicon.png"]),
 	mimeTypes: {".png":"image/png"},
 	_: {
-		client: {"start":{"file":"_app/immutable/entry/start.c9283d4b.js","imports":["_app/immutable/entry/start.c9283d4b.js","_app/immutable/chunks/index.b1f5afdd.js","_app/immutable/chunks/index.8ff6998b.js"],"stylesheets":[],"fonts":[]},"app":{"file":"_app/immutable/entry/app.97bf8bba.js","imports":["_app/immutable/entry/app.97bf8bba.js","_app/immutable/chunks/index.b1f5afdd.js"],"stylesheets":[],"fonts":[]}},
+		client: {"start":{"file":"_app/immutable/entry/start.be9e80f8.js","imports":["_app/immutable/entry/start.be9e80f8.js","_app/immutable/chunks/index.b1f5afdd.js","_app/immutable/chunks/index.8ff6998b.js"],"stylesheets":[],"fonts":[]},"app":{"file":"_app/immutable/entry/app.97bf8bba.js","imports":["_app/immutable/entry/app.97bf8bba.js","_app/immutable/chunks/index.b1f5afdd.js"],"stylesheets":[],"fonts":[]}},
 		nodes: [
 			() => import('./nodes/0.js'),
 			() => import('./nodes/1.js')
