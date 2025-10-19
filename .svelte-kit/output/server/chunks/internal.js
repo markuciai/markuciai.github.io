7f830b63dd63f8295c3a73051c4fd0f7c9df7fd1
Čhepulis <chepulis@gmail.com>
2025-10-19 10:50:56 +0300

    broken stuff pre big upgrade

--- .svelte-kit/output/server/chunks/internal.js
+++ .svelte-kit/output/server/chunks/internal.js
@@ -171,7 +171,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "1907tvi"
+  version_hash: "1izgazg"
 };
 function get_hooks() {
   return {};
--- .svelte-kit/output/server/manifest.js
+++ .svelte-kit/output/server/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set([".DS_Store","favicon.png"]),
 	mimeTypes: {".png":"image/png"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.92c91bdc.js","app":"_app/immutable/entry/app.f70d020b.js","imports":["_app/immutable/entry/start.92c91bdc.js","_app/immutable/chunks/index.91a1003d.js","_app/immutable/chunks/index.1ceca79a.js","_app/immutable/entry/app.f70d020b.js","_app/immutable/chunks/index.91a1003d.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.021c4173.js","app":"_app/immutable/entry/app.f70d020b.js","imports":["_app/immutable/entry/start.021c4173.js","_app/immutable/chunks/index.91a1003d.js","_app/immutable/chunks/index.1ceca79a.js","_app/immutable/entry/app.f70d020b.js","_app/immutable/chunks/index.91a1003d.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js'))
