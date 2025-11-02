c68e6338a6e1316809f78daf51bb4aa54cbbb823
Čhepulis <chepulis@gmail.com>
2025-11-02 21:06:22 +0200

    testing scroll fix

--- .svelte-kit/output/server/chunks/internal.js
+++ .svelte-kit/output/server/chunks/internal.js
@@ -2651,7 +2651,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "xrkpff"
+  version_hash: "eig5w7"
 };
 async function get_hooks() {
   let handle;
--- src/components/The_map.svelte
+++ src/components/The_map.svelte
@@ -151,14 +151,51 @@ if (browser) {
     // show_position()
     // watch_position()
     console.log("i'm a map")
-}
+    fixScrollUpdateSafariIOs()
 
+
+}
 
 
 
 onMount(async () => { 
     // document.getElementsBy
 });
+
+
+
+
+
+
+
+function fixScrollUpdateSafariIOs() {
+    const isIosSafari = navigator.userAgent.match(/(iPod|iPhone|iPad)/) && navigator.userAgent.match(/AppleWebKit/);
+    // Check if it's an iOS device and Safari
+    const isMobileSafari = isIosSafari && window.innerWidth < 768;
+    if (isMobileSafari) {
+        (function () {
+            // Create a hidden log div
+            const logDiv = document.createElement('div');
+            logDiv.style.height = '0px'; // Set the height to 0 pixels
+            logDiv.style.overflow = 'hidden'; // Hide the content
+            document.body.appendChild(logDiv);
+
+            // Function to update the log with the scroll position
+            function updateLog() {
+                logDiv.innerHTML = window.scrollY.toFixed(0);
+            }
+
+            // Add listeners for scroll and touch events
+            window.addEventListener('scroll', updateLog, { passive: true, capture: true });
+            window.addEventListener('touchstart', updateLog, { passive: true, capture: true });
+            window.addEventListener('touchmove', updateLog, { passive: true, capture: true });
+            window.addEventListener('touchend', updateLog, { passive: true, capture: true });
+        })();
+    }
+}
+
+
+
 
 
 
