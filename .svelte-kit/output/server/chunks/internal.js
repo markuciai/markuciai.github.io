1c4f1c5ee9a3688596dd9b42bc2af063e33cafe9
Čhepulis <chepulis@gmail.com>
2025-11-02 23:05:24 +0200

    y no sticky build

--- .svelte-kit/output/server/chunks/internal.js
+++ .svelte-kit/output/server/chunks/internal.js
@@ -2651,7 +2651,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "1v05zlf"
+  version_hash: "ne98wm"
 };
 async function get_hooks() {
   let handle;
