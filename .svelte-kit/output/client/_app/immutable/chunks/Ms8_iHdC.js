8421557d3152d77e08b8f33071be72971120b2a9
Čhepulis <chepulis@gmail.com>
2025-11-02 23:29:43 +0200

    fixed sticky and z-index bugs (build?)

---
+++ .svelte-kit/output/client/_app/immutable/chunks/Ms8_iHdC.js
@@ -0,0 +1,1 @@
+const s=""+new URL("../assets/head_signs.CJk7746j.png",import.meta.url).href;export{s as h};
---
+++ .svelte-kit/output/server/manifest.js
@@ -0,0 +1,32 @@
+export const manifest = (() => {
+function __memo(fn) {
+	let value;
+	return () => value ??= (value = fn());
+}
+
+return {
+	appDir: "_app",
+	appPath: "_app",
+	assets: new Set(["favicon.png","ornament.png","robots.txt"]),
+	mimeTypes: {".png":"image/png",".txt":"text/plain"},
+	_: {
+		client: {start:"_app/immutable/entry/start.rVAx1cqw.js",app:"_app/immutable/entry/app.BLwp8Y1Z.js",imports:["_app/immutable/entry/start.rVAx1cqw.js","_app/immutable/chunks/CvUsXtnB.js","_app/immutable/chunks/DzEbYLO3.js","_app/immutable/entry/app.BLwp8Y1Z.js","_app/immutable/chunks/CvUsXtnB.js","_app/immutable/chunks/BzR0kroy.js","_app/immutable/chunks/C0aZwFk8.js","_app/immutable/chunks/DzEbYLO3.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		nodes: [
+			__memo(() => import('./nodes/0.js')),
+			__memo(() => import('./nodes/1.js'))
+		],
+		remotes: {
+			
+		},
+		routes: [
+			
+		],
+		prerendered_routes: new Set(["/","/lobis","/lobis/arklides_1935","/lobis/cerkve_1903","/lobis/duonkepe_1868","/lobis/finish_1935","/lobis/kroketas_1867","/lobis/kryzius_1880","/lobis/namelis_1855","/lobis/oranzerija_1875","/lobis/start_1865","/lobis/sulinys_1867","/lobis/suniukai-1883","/lobis/tvenkiniai_1917","/lobis/virtuve_1899","/lobis/vistide_1877"]),
+		matchers: async () => {
+			
+			return {  };
+		},
+		server_assets: {}
+	}
+}
+})();
